9780bdfd6b69040493cb419e0788420ef8fa5669
T-guo <136016593@qq.com>
2019-02-21 16:50:05 +0800

    no message

---
+++ DemoTest/DemoTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoTest
+//
+//  Created by T-guo on 2019/2/21.
+//  Copyright © 2019 T_Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoTest/DemoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoTest
+//
+//  Created by T-guo on 2019/2/21.
+//  Copyright © 2019 T_Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
