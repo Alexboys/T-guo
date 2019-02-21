7987f9a7ff7213ddbbf08452f55491f9ae7d24e4
T-guo <136016593@qq.com>
2019-02-21 18:35:39 +0800

    no message

--- DemoTest/DemoTest/ViewController.m
+++ DemoTest/DemoTest/ViewController.m
@@ -14,9 +14,10 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 
